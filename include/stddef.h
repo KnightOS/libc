1e8c7975a1d3f1f512a5aa7f6033e531545fed60
Drew DeVault <ddevault@hosting.com>
2014-11-21 16:05:30 -0700

    Add some standard headers

---
+++ include/stddef.h
@@ -0,0 +1,10 @@
+#ifndef __STDDEF_H
+#define __STDDEF_H
+
+typedef unsigned short size_t;
+typedef unsigned short nullptr_t;
+typedef unsigned short ptrdiff_t;
+typedef unsigned short wchar_t;
+#define NULL 0
+
+#endif
