91650653ab3f7306914c8727097fea623726e7d2
Drew DeVault <sir@cmpwn.com>
2015-04-22 14:37:20 -0600

    Implement draw_sprite and fix draw_char

--- include/knightos/display.h
+++ include/knightos/display.h
@@ -7,6 +7,7 @@ SCREEN *screen_allocate();
 void screen_clear(SCREEN *screen);
 void screen_draw(SCREEN *screen);
 void draw_string(SCREEN *screen, unsigned char x, unsigned char y, const char *string);
+void draw_sprite(SCREEN *screen, unsigned char x, unsigned char y, unsigned char height, const void *sprite);
 void draw_char(SCREEN *screen, unsigned char x, unsigned char y, unsigned char value);
 void draw_short(SCREEN *screen, unsigned char x, unsigned char y, unsigned short value);
 void set_left_margin(unsigned char margin);
