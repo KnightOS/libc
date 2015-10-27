377ccc9f2fa2e4c38b59064ec9dacea65c1e817c
Aaron Ouellette <aouellette2016@gmail.com>
2015-10-26 22:01:12 -0400

    add some useful constants to math.h

--- include/math.h
+++ include/math.h
@@ -1,7 +1,21 @@
 #ifndef __MATH_H
 #define __MATH_H
+/* Provides most of the standard C math.h routines */
 
-//Provides most of the standard C math.h routines
+// define some constants
+#define M_E              2.718281828    /* e */
+#define M_LOG2E	         1.442695040    /* log_2 e */
+#define M_LOG10E         0.434294481    /* log_10 e */
+#define M_LN2            0.693147180    /* log_e 2 */
+#define M_LN10           2.302585092    /* log_e 10 */
+#define M_PI             3.141592653	/* pi */
+#define M_PI_2           1.570796326	/* pi/2 */
+#define M_PI_4           0.785398163	/* pi/4 */
+#define M_1_PI           0.318309886	/* 1/pi */
+#define M_2_PI           0.636619772	/* 2/pi */
+#define M_2_SQRTPI       1.128379167	/* 2/sqrt(pi) */
+#define M_SQRT2          1.414213562	/* sqrt(2) */
+#define M_SQRT1_2        0.707106781	/* 1/sqrt(2) */
 
 int abs(int);
 double fabs(double);
