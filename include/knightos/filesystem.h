f4285b60f1afeb52abedb7b4b1b3405d5860a2ec
Max Leiter <mleiter18@priorypanther.com>
2015-12-02 12:41:49 -0800

    exists, not exist for filesystem.h, no C-wrapper for fix and check filesystem

--- include/knightos/filesystem.h
+++ include/knightos/filesystem.h
@@ -2,13 +2,11 @@
 #define __FILESYSTEM_H
 
 #include <stdbool.h>
-unsigned char check_filesystem();
 void create_directory(const char *path);
 void create_symlink(const char *path, const char *target_path);
 void delete_file(const char *path);
-bool directory_exist(const char *path);
-bool file_exist(const char *path);
-void fix_filesystem();
+bool directory_exists(const char *path);
+bool file_exists(const char *path);
 void rename_file(const char *path, const *char name);
 
 #endif
