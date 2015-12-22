4aea57ad40a24a4e290cf5179fb54310c4c5b0d8
Max Leiter <mleiter18@priorypanther.com>
2015-12-22 12:44:42 -0600

    Conventions

--- include/stdio.h
+++ include/stdio.h
@@ -8,6 +8,6 @@ int sprintf(char *str, const char *format, ...);
 int snprintf(char *str, size_t size, const char *format, ...);
 int vsprintf(char *str, const char *format, va_list args);
 int vsnprintf(char *str, size_t size, const char *format, va_list args);
-int asprintf(char **strp, const char *fmt, ...);
-int vasprintf(char **strp, const char *fmt, va_list ap);
+int asprintf(char **str, const char *format, ...);
+int vasprintf(char **str, const char *format, va_list args);
 #endif
