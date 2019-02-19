9923cf4a45b45e024a121afb7614f3870f4a71d8
Drew DeVault <sir@cmpwn.com>
2019-02-18 20:37:48 -0500

    Fix broken strncmp implementation

--- src/string.c
+++ src/string.c
@@ -101,11 +101,11 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-	while (n-- && *s1 && *s1 == *s2) {
-		++s1;
-		++s2;
+	if (!n--) {
+		return 0;
 	}
-	return (int)*s1 - (int)*s2;
+	for (; *s1 && *s2 && n && *s1 == *s2; s1++, s2++, n--);
+	return *s1 - *s2;
 }
 
 char *strcat(char *dst, char *src) {
