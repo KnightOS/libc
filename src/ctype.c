d3217a603174e22623e95c9ee2454d35b77b5ff5
Noam Preil <noam@pixelhero.dev>
2021-10-23 20:04:09 -0400

    ctype: fix tolower and toupper

--- src/ctype.c
+++ src/ctype.c
@@ -1,48 +1,63 @@
 #include <ctype.h>
 #include <string.h>
 
-int tolower(int c) {
-	if ('A' <= c && c <= 'Z') {
-		return c - ('a' - 'A');
-	}
+int
+tolower(int c)
+{
+	if(isupper(c))
+		return c + ('a' - 'A');
 	return c;
 }
 
-int toupper(int c) {
-	if ('a' <= c && c <= 'z') {
-		return c - ('A' - 'a');
-	}
+int
+toupper(int c)
+{
+	if(islower(c))
+		return c + ('A' - 'a');
 	return c;
 }
 
-int isspace(int c) {
-	if (c == ' ' || c == '\t') {
+int
+isspace(int c)
+{
+	if(c == ' ' || c == '\t')
 		return 1;
-	}
 	return 0;
 }
 
-int islower(int c) {
+int
+islower(int c)
+{
 	return c >= 'a' && c <= 'z';
 }
 
-int isupper(int c) {
+int
+isupper(int c)
+{
 	return c >= 'A' && c <= 'Z';
 }
 
-int toascii(int c) {
-	return c;
-}
-
-int isdigit(int c) {
+int
+isdigit(int c)
+{
 	return c >= '0' && c <= '9';
 }
 
-int isalpha(int c) {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z');
+int
+isalpha(int c)
+{
+	return islower(c) || isupper(c);
 }
 
-int isalnum(int c) {
+int
+isalnum(int c)
+{
 	return isdigit(c) || isalpha(c);
+}
+
+int
+toascii(int c)
+{
+	return c & 0x7F;
 }
 
